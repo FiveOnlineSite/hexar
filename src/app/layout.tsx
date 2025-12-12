c027d82ee1a6060466a0d5784178a60b5d324183
Mansi <mansi.fiveonline@gmail.com>
2025-12-12 14:40:24 +0530

    sliders

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export const metadata: Metadata = {
   icons: {
     icon: "/images/icons/favicon.png",
   },
+   viewport: "width=device-width, initial-scale=1.0",
 };
 
 export default function RootLayout({
