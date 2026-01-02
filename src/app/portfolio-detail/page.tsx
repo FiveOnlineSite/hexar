d22e507e1d615b12e558687ffaadf980016e92dd
Mansi <mansi.fiveonline@gmail.com>
2026-01-02 13:21:09 +0530

    added new page

--- src/app/portfolio-detail/page.tsx
+++
@@ -1,9 +0,0 @@
-import PortfolioDetailBanner from "@/src/components/PortfolioDetail/PortfolioDetailBanner";
-
-export default function PortfolioDetail (){
-    return (
-        <>
-        <PortfolioDetailBanner/>
-        </>
-    )
-}
