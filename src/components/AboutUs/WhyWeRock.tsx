da49ba53440111cfc0f52518ca3aecce45b12533
Mansi <mansi.fiveonline@gmail.com>
2026-01-21 17:13:55 +0530

    images added

--- src/components/AboutUs/WhyWeRock.tsx
+++ src/components/AboutUs/WhyWeRock.tsx
@@ -1,3 +1,5 @@
+import { whyus } from "@/src/data/whyus";
+
 export default function WhyWeRock() {
     return (
         <section className="bg-[#0A0A0A] why-we-rock-section lg:p-16 md:p-16 p-8 border-white border-t">
@@ -11,51 +13,16 @@ export default function WhyWeRock() {
         </p>
             </div>
         <div className="reveal-stagger grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 grid-col-1 gap-4 lg:my-12 md:my-10 my-8">
-            <div className="group mb-2 relative bg-black rounded-lg overflow-hidden h-full transition-all duration-700"> 
-                    <img src="./images/why-us-2.png" className="w-full xl:object-cover lg:object-cover object-cover xl:h-full h-[255px] relative overflow-hidden" /> 
-                    <div className="xl:absolute relative 3xl:-bottom-[32%] 2xl:-bottom-[35%] xl:-bottom-[42%] bottom-0 xl:group-hover:bottom-[0%] transition-all duration-700 ease-out p-2 bg-black"> 
-                        <h2 className="3xl:text-[30px] 2xl:text-[25px] xl:text-[22px] lg:text-[22px] md:text-[20px] text-[18px] font-semibold leading-tight pb-2">Artistic Mastery </h2> 
-                        <p className="3xl:text-[20px] 2xl:text-[18px] xl:text-[15px] lg:text-[15px] md:text-[15px] text-[14px] font-light leading-base pb-4"> Our team blends world-class artistry with deep technical expertise, delivering 3D work that is both visually stunning and production-ready. 
-                        </p> 
+            {whyus.map((item, index) => (
+                <div key={index} className="group mb-2 relative bg-black rounded-lg overflow-hidden h-full transition-all duration-700"> 
+                    <img src={item.image} className="w-full xl:object-cover lg:object-cover object-cover xl:h-full h-[255px] relative overflow-hidden" /> 
+                    <div className="xl:absolute relative 3xl:-bottom-[25%] 2xl:-bottom-[30%] xl:-bottom-[32%] bottom-0 xl:group-hover:bottom-[0%] transition-all duration-700 ease-out p-2 bg-black"> 
+                        <h2 className="3xl:text-[30px] 2xl:text-[25px] xl:text-[22px] lg:text-[22px] md:text-[20px] text-[18px] font-semibold leading-tight pb-2">{item.title}</h2> 
+                        <p className="3xl:text-[18px] 2xl:text-[18px] xl:text-[14px] lg:text-[14px] md:text-[12px] text-[12px] 3xl:min-h-[100px] 2xl:min-h-[100px] xl:min-h-[80px] font-light leading-base pb-4">{item.content}</p>
                     </div> 
             </div>
-
-             <div className="group mb-2 relative bg-black rounded-lg overflow-hidden h-full transition-all duration-700"> 
-                    <img src="./images/why-us-2.png" className="w-full xl:object-cover lg:object-cover object-cover xl:h-full h-[255px] relative overflow-hidden" /> 
-                    <div className="xl:absolute relative 3xl:-bottom-[32%] 2xl:-bottom-[35%] xl:-bottom-[42%] bottom-0 xl:group-hover:bottom-[0%] transition-all duration-700 ease-out p-2 bg-black"> 
-                        <h2 className="3xl:text-[30px] 2xl:text-[25px] xl:text-[22px] lg:text-[22px] md:text-[20px] text-[18px] font-semibold leading-tight pb-2">Artistic Mastery </h2> 
-                        <p className="3xl:text-[20px] 2xl:text-[18px] xl:text-[15px] lg:text-[15px] md:text-[15px] text-[14px] font-light leading-base pb-4"> Our team blends world-class artistry with deep technical expertise, delivering 3D work that is both visually stunning and production-ready. 
-                        </p> 
-                    </div> 
-            </div>
-
-             <div className="group mb-2 relative bg-black rounded-lg overflow-hidden h-full transition-all duration-700"> 
-                    <img src="./images/why-us-2.png" className="w-full xl:object-cover lg:object-cover object-cover xl:h-full h-[255px] relative overflow-hidden" /> 
-                    <div className="xl:absolute relative 3xl:-bottom-[32%] 2xl:-bottom-[35%] xl:-bottom-[42%] bottom-0 xl:group-hover:bottom-[0%] transition-all duration-700 ease-out p-2 bg-black"> 
-                        <h2 className="3xl:text-[30px] 2xl:text-[25px] xl:text-[22px] lg:text-[22px] md:text-[20px] text-[18px] font-semibold leading-tight pb-2">Artistic Mastery </h2> 
-                        <p className="3xl:text-[20px] 2xl:text-[18px] xl:text-[15px] lg:text-[15px] md:text-[15px] text-[14px] font-light leading-base pb-4"> Our team blends world-class artistry with deep technical expertise, delivering 3D work that is both visually stunning and production-ready. 
-                        </p> 
-                    </div> 
-            </div>
-
-             <div className="group mb-2 relative bg-black rounded-lg overflow-hidden h-full transition-all duration-700"> 
-                    <img src="./images/why-us-2.png" className="w-full xl:object-cover lg:object-cover object-cover xl:h-full h-[255px] relative overflow-hidden" /> 
-                    <div className="xl:absolute relative 3xl:-bottom-[32%] 2xl:-bottom-[35%] xl:-bottom-[42%] bottom-0 xl:group-hover:bottom-[0%] transition-all duration-700 ease-out p-2 bg-black"> 
-                        <h2 className="3xl:text-[30px] 2xl:text-[25px] xl:text-[22px] lg:text-[22px] md:text-[20px] text-[18px] font-semibold leading-tight pb-2">Artistic Mastery </h2> 
-                        <p className="3xl:text-[20px] 2xl:text-[18px] xl:text-[15px] lg:text-[15px] md:text-[15px] text-[14px] font-light leading-base pb-4"> Our team blends world-class artistry with deep technical expertise, delivering 3D work that is both visually stunning and production-ready. 
-                        </p> 
-                    </div> 
-            </div>
-
-             <div className="group mb-2 relative bg-black rounded-lg overflow-hidden h-full transition-all duration-700"> 
-                    <img src="./images/why-us-2.png" className="w-full xl:object-cover lg:object-cover object-cover xl:h-full h-[255px] relative overflow-hidden" /> 
-                    <div className="xl:absolute relative 3xl:-bottom-[32%] 2xl:-bottom-[35%] xl:-bottom-[42%] bottom-0 xl:group-hover:bottom-[0%] transition-all duration-700 ease-out p-2 bg-black"> 
-                        <h2 className="3xl:text-[30px] 2xl:text-[25px] xl:text-[22px] lg:text-[22px] md:text-[20px] text-[18px] font-semibold leading-tight pb-2">Artistic Mastery </h2> 
-                        <p className="3xl:text-[20px] 2xl:text-[18px] xl:text-[15px] lg:text-[15px] md:text-[15px] text-[14px] font-light leading-base pb-4"> Our team blends world-class artistry with deep technical expertise, delivering 3D work that is both visually stunning and production-ready. 
-                        </p> 
-                    </div> 
-            </div>
-
+                ))}
+            
             </div>
         </section>
     )
