a7fb532cdb0e8abe8868d84f7e8b04f9aa1f13d4
Mansi <mansi.fiveonline@gmail.com>
2026-01-21 12:51:21 +0530

    content update

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,27 +1,21 @@
 
 export const projects = [
     {
+        title: "GHOSTBUSTERS",
+        date: "24 Nov",
+        image: "/images/portfolio-3.png",
+        content: "Spirits Unleashed",
+     },
+     {
+        title: "PREDATORS: HUNTING GROUNDS",
+        date: "24 Nov",
+        image: "/images/portfolio-2.png",
+        content: "Predator Skin. Character Skins, Props and Weapons",
+     },
+     {
         title: "Back 4 Blood",
         date: "24 Nov",
         image: "/images/portfolio-1.png",
-        content: "NPC Characters (Modular), HairStyles, Head & Zombie Body Parts",
-     },
-     {
-        title: "Back 4 Blood",
-        date: "24 Nov",
-        image: "/images/portfolio-2.png",
-        content: "NPC Characters (Modular), HairStyles, Head & Zombie Body Parts",
-     },
-     {
-        title: "Back 4 Blood",
-        date: "24 Nov",
-        image: "/images/portfolio-3.png",
-        content: "NPC Characters (Modular), HairStyles, Head & Zombie Body Parts",
-     },
-     {
-        title: "Back 4 Blood",
-        date: "24 Nov",
-        image: "/images/portfolio-4.png",
         content: "NPC Characters (Modular), HairStyles, Head & Zombie Body Parts",
      },
     
